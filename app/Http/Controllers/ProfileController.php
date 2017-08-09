638f8bcae68388ca065d82ea2adc85596abdf2e1
Max Bosse <max@bosse.io>
2017-08-09 18:24:12 +0200

    Revert "fix p/w reset"
    This reverts commit c7cdce9f388fbf5c61aec7d045bb3bba5f7d774b.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -273,8 +273,8 @@ class ProfileController extends Controller
             if(Input::get('new_password') == Input::get('new_password_confirmed'))
             {
                 //Auth::user()->update(['password', Hash::make(Input::get('new_password'))]);
-                $user_id = Auth::User()->id;
-                $obj_user = User::find(Auth::id());
+                $user_id = Auth::User()->id; 
+                $obj_user = User::find($user_id);
                 $obj_user->password = Hash::make(Input::get('new_password'));;
                 $obj_user->save();
                 return redirect()->back()->with('success', 'Your password was changed!');
