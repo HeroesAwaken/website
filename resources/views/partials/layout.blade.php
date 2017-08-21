5f4fee011e8b62e7ae69f33f4c1ea41b74133520
Max Bosse <max@bosse.io>
2017-08-21 02:10:11 +0200

    no download icon

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -1,6 +1,5 @@
 @include('partials.header')
 
-@if( ! Request::is('admin*'))<a href="{{ route('download') }}"><img class="hide-for-small-only" style="position: absolute; left: 1rem; top: 8rem; z-index:2500; width: 10rem;" src="{{ asset('images/dl_new.png') }}"></a>@endif
 
 @if( ! Request::is('admin*'))
     @include('partials.navbar')
